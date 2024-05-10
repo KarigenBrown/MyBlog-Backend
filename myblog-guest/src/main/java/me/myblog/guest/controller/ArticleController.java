90a881cec52e5be7a354745578b74e84a981b6d6
KarigenBrown <karigenbrown@gmail.com>
2024-05-10 16:09:05 +0800

    添加CORS

--- myblog-guest/src/main/java/me/myblog/guest/controller/ArticleController.java
+++ myblog-guest/src/main/java/me/myblog/guest/controller/ArticleController.java
@@ -1,5 +1,8 @@
 package me.myblog.guest.controller;
 
+import me.myblog.framework.domain.Response;
+import me.myblog.framework.domain.jooq.tables.pojos.Article;
+import me.myblog.framework.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +13,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
+    @Autowired
+    private ArticleService articleService;
 
-
-
+    // @GetMapping("/hotArticles")
+    @GetMapping("/hotArticleList")
+    public Response<List<Article>> getHotArticles() {
+        return Response.ok(articleService.getHotArticles());
+    }
 }
